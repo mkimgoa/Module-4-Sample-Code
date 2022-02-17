77ec5b3723258b29763a8abfc6a953e4747ac1f3
Margaret Kim <mkim@asij.ac.jp>
2022-02-17 13:36:41 +0900

    edited screen size

--- filereading.py
+++ filereading.py
@@ -4,7 +4,7 @@ f = open("sampledata.txt","r")
 aList = []
 bList = []
 for line in f:
-    line = line.strip()
+    line = line.strip() # removes whitespace and newline characters at the beginning/end of the line
     letter, number = line.split(",")
     number = int(number)
     if letter == "a":
